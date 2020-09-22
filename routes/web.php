6dd52c2292d02334b47693f516e3b563ea8ca5fd
Arthur Alves <art4321@outlook.com.br>
2020-09-22 18:34:00 -0300

    tela criar-campanha

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,9 @@ Route::get('/', function () {
 Route::get('/depoimentos', function () {
     return view('depoimento');
 })->name('view.depoimento');
-Route::get('/doacoes', function() {
+Route::get('/doacoes', function () {
     return view('doacoes');
 });
+Route::get('/criar-campanhas', function () {
+    return view('criar-campanha');
+})->name('view.criar-campanha');
